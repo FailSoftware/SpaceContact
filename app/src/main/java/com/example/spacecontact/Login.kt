f1fbf846f22cc11900695867b84e5425c2097dad
angiemoliee <48281415+angiemoliee@users.noreply.github.com>
2019-11-07 19:12:55 +0100

    Login method
    Login harcoded

--- app/src/main/java/com/example/spacecontact/Login.kt
+++ app/src/main/java/com/example/spacecontact/Login.kt
@@ -1,12 +1,39 @@
 package com.example.spacecontact
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
 
 class Login : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+    }
+
+
+
+
+    fun toCharacter(view: View){
+
+    var name = findViewById(R.id.etUser) as EditText
+        var sName = name.getText().toString()
+
+        var pass = findViewById(R.id.etPass) as EditText
+        var sPass = pass.getText().toString()
+
+        if (sName.equals("user") && sPass.equals("user")) {
+
+            val intent = Intent(this, Characters::class.java)
+            startActivity(intent)
+
+        } else {
+            Toast.makeText(this, "No es correcto", Toast.LENGTH_SHORT).show()
+        }
+
+
     }
 }
