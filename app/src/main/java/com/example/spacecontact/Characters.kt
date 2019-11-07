39a493d11b2b9a7b2166d0eec25ec11224cd1101
Juan Miguel <kitsu_pkiller@hotmail.com>
2019-11-07 18:26:42 +0100

    Edited sprites, different body, suit, and beards to make the customization character

--- app/src/main/java/com/example/spacecontact/Characters.kt
+++ app/src/main/java/com/example/spacecontact/Characters.kt
@@ -1,5 +1,6 @@
 package com.example.spacecontact
 
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -8,5 +9,8 @@ class Characters : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_characters)
+
+        // val icon = BitmapFactory.decodeResource(this.getResources(), android.R.drawable.)
+
     }
 }
