b0ca7af601f0e209608595117f2ee6a385b8338e
Juan Miguel <kitsu_pkiller@hotmail.com>
2019-11-07 15:52:39 +0100

    MainPush
    Pushed the new project

---
+++ app/src/main/java/com/example/spacecontact/Game.kt
@@ -0,0 +1,12 @@
+package com.example.spacecontact
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Game : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game)
+    }
+}
