9f86917a85b1266c2c85da50a349949e95f4ad2b
BalintDAM <48281569+BalintDAM@users.noreply.github.com>
2019-11-07 17:13:56 +0100

    Load login & action bar
    > Intent from MainActivity to Login
    > Removed action bar

--- app/src/main/java/com/example/spacecontact/MainActivity.kt
+++ app/src/main/java/com/example/spacecontact/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.spacecontact
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -8,5 +9,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //TODO que alguien ponga que salga el logo de la empresa aqui 3 segundos, porfaplz :3
+        loadLogin()
     }
+
+    fun loadLogin(){
+     var intent: Intent = Intent(this, Login::class.java)
+        startActivity(intent)
+
+    }
+
 }
