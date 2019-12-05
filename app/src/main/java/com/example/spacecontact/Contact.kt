7a86f7069aeb82c27c3f0b2fb58d1ae1743360d3
angiemoliee <48281415+angiemoliee@users.noreply.github.com>
2019-12-05 21:38:04 +0100

    - Contact Activity desing - Social Network conections - Added vector icons for Twitter, Call, and Web. - Add fuction to access via button to RepoWeb and Twitter of FailSoft - Added different Strings in res/values/strings/stringsEN - Add fuction in Contact.kt to ask permissions and make call after accept  permissions. - Add function to make user alerted if not accept permissions - Add a litle description about us. - More minor changes.

--- app/src/main/java/com/example/spacecontact/Contact.kt
+++ app/src/main/java/com/example/spacecontact/Contact.kt
@@ -1,12 +1,79 @@
 package com.example.spacecontact
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest.permission.CALL_PHONE
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.View
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
+
 
 class Contact : Login() {
+
+    private val PERMISO_LLAMADA = 3
+    private val i = Intent(Intent.ACTION_CALL, Uri.parse("tel:952312703"))
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_contact)
     }
+
+    fun toWeb(view: View) {
+        val uri =
+            Uri.parse("https://github.com/FailSoftware/SpaceContact") // missing 'http://' will cause crashed
+        val intent = Intent(Intent.ACTION_VIEW, uri)
+        startActivity(intent)
+    }
+
+    fun toSocial(view: View) {
+        val uri = Uri.parse("https://twitter.com/home") // missing 'http://' will cause crashed
+        val intent = Intent(Intent.ACTION_VIEW, uri)
+        startActivity(intent)
+    }
+
+
+    fun alertPermission() {
+
+        var title : String = this.getString(R.string.alertTitlePerm)
+        var msg : String = this.getString(R.string.alertMsgPerm)
+        val bdr = AlertDialog.Builder(this)
+        bdr.setTitle(title)
+        bdr.setMessage(msg)
+        bdr.create()
+        bdr.show()
+    }
+
+    fun toCall(view: View) {
+
+        if (ActivityCompat.checkSelfPermission(this, CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+
+            if (!ActivityCompat.shouldShowRequestPermissionRationale(this, CALL_PHONE)) {
+                alertPermission()
+            }else{
+                ActivityCompat.requestPermissions(this, arrayOf(CALL_PHONE), PERMISO_LLAMADA)
+            }
+        } else {
+            startActivity(i)
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<String>, grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == PERMISO_LLAMADA) {
+            if (grantResults.size == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                startActivity(i)
+            }
+        }
+    }
 }
+
+
+
+
