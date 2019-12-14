81ffc5b16cf553856e90898345db57cf6c6d8472
Juan Miguel <kitsu_pkiller@hotmail.com>
2019-12-14 05:43:31 +0100

    Code Changes: - Added new adapter for healing list, now if Heal button is pressed you can heal workers :) - Added more strings in values. - Implemented fun adapter in game.

---
+++ app/src/main/java/com/example/spacecontact/gameFunctions/AdapterHeal.kt
@@ -0,0 +1,72 @@
+package com.example.spacecontact.gameFunctions
+
+import android.app.Activity
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.TextView
+import com.example.spacecontact.R
+import com.example.spacecontact.entity.Worker
+
+class AdapterHeal constructor(context: Context, workerHealing : ArrayList<Worker>):BaseAdapter() {
+    private var context : Context = context
+    private var workerHealing : ArrayList<Worker> = workerHealing
+    private lateinit var hpW : String
+    private lateinit var wName: String
+    private lateinit var wPr: String
+
+
+    override fun getCount(): Int {return workerHealing.size}
+    override fun getItem(position: Int): Any {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemId(position: Int): Long {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        var v : View? = convertView
+        var lInflater: LayoutInflater = (context as Activity).layoutInflater
+        v = lInflater.inflate(R.layout.adapter_healin, null)
+        v.setTag(position)
+
+
+
+        var wNameAda : TextView = v.findViewById(R.id.tvWname)
+        var wHpAda : TextView = v.findViewById(R.id.tvWhp)
+        var wProbAda : TextView = v.findViewById(R.id.tvWp)
+
+
+        hpW = context.getString(R.string.shipHp)
+        wName = context.getString(R.string.wNameAd)
+
+        wPr =""
+
+        var contextHealing: Worker
+        contextHealing = workerHealing.get(position)
+
+        wNameAda.setText(context.getString(R.string.wNameAd)+": "+contextHealing.name)
+        wHpAda.setText(contextHealing.currentHealth.toString() +"/"+contextHealing.totalHealth)
+        if(contextHealing.onFire){
+            wPr += " ["+context.getString(R.string.fire)+"] "
+
+        }
+
+        if(contextHealing.onShock){
+            wPr += " ["+context.getString(R.string.shock)+"] "
+        }
+
+        if(contextHealing.wounded){
+            wPr += " ["+context.getString(R.string.wounded)+"] "
+
+        }
+        wProbAda.setText(context.getString(R.string.shipProblem)+": "+wPr)
+
+        return v
+
+
+    }
+}
